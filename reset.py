486fb4a8fd7e01183e2cdcc17ebe3e3ef50cb3b7
Julie319 <96014979+Julie319@users.noreply.github.com>
2021-12-12 17:02:25 +0100

    reset.py
    move all servos to position percentage 0

---
+++ reset.py
@@ -0,0 +1,14 @@
+from Robot import Robot
+robot = Robot()
+
+robot.servo.move_servo_to_percent(0,0)
+
+robot.servo.move_servo_to_percent(1,0)
+
+robot.servo.move_servo_to_percent(2,0)
+
+robot.servo.move_servo_to_percent(3,0)
+
+robot.servo.move_servo_to_percent(4,0)
+
+robot.servo.move_servo_to_percent(5,0)
